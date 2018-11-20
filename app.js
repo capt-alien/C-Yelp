eb98c2a2dbe4dd563daba9a83bb366f0ecc90c36
capt-alien <eric.botcher@presidio.edu>
2018-11-19 17:47:23 -0800

    added mongo DB routting

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/c-yelp');
+const bodyParser = require('body-parser');
+//ODM
+const Charity = mongoose.model('Charity', {
+  gift: Number,
+  charName: String,
+  description: String
+
+});
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.js
 var exphbs = require('express-handlebars');
@@ -7,12 +20,42 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// OUR MOCK ARRAY OF PROJECTS
+// let charities = [
+//   { gift: "Great Review", CharName: "Batman II" },
+//   { gift: "Awesome Movie", CharName: "Titanic" }
+// ]
 
 
 
-app.get('/',(req, res) => {
-    res.render('home', { msg:'Handlebars are fucking dope!'});
+
+
+// INDEX
+app.get('/', (req, res) => {
+  Charity.find()
+    .then(charities => {
+      res.render('charities-index', { charities: charities });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
+
+// NEW
+app.get('/charities/new', (req, res) => {
+  res.render('charities-new', {});
+})
+
+// CREATE
+app.post('/charities', (req, res) => {
+  Charity.create(req.body).then((charity) => {
+    console.log(charity);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+})
+})
+
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
