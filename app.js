88cf7e3ae2798f1e54c3504cd158cce4ddd2e3e3
capt-alien <eric.botcher@presidio.edu>
2018-11-28 14:22:42 -0800

    users can see comments

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 const express = require('express')
-const methodOverride = require('method-override')
 const app = express()
-var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/c-yelp-eb');
-const bodyParser = require('body-parser');
 
-//ODM
-app.use(methodOverride('_method'))
+const methodOverride = require('method-override')
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/c-yelp-eb', {
+	useNewUrlParser: true
+});
+
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.js
 var exphbs = require('express-handlebars');
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// Routes
 const Charities = require('./controllers/charities')(app);
 
-// export for testing
-module.exports = app;
 
 const port = process.env.PORT || 3000;
 
 app.listen( port, () => {
   console.log('App listening on port 3000!')
 })
+
+// export for testing
+module.exports = app;
