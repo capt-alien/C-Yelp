a4da4a9d9d039e310b1312bf7a49c73b8bb86235
capt-alien <eric.botcher@presidio.edu>
2018-11-28 13:18:14 -0800

    works local but not heroku

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/c-yelp');
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/c-yelp-eb');
 const bodyParser = require('body-parser');
+
 //ODM
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
 
 // app.js
 var exphbs = require('express-handlebars');
@@ -21,6 +20,8 @@ const Charities = require('./controllers/charities')(app);
 // export for testing
 module.exports = app;
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen( port, () => {
   console.log('App listening on port 3000!')
 })
